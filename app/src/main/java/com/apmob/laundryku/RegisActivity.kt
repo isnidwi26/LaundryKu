94a12f9ea389d9259293da668982359d7112eb88
isnidwi26 <isnidwitiniardi26@gmail.com>
2021-06-10 22:10:45 +0700

    nambah ke firestore
    input data ke firestore

--- app/src/main/java/com/apmob/laundryku/RegisActivity.kt
+++ app/src/main/java/com/apmob/laundryku/RegisActivity.kt
@@ -16,7 +16,7 @@ import com.google.firebase.ktx.Firebase
 class RegisActivity : AppCompatActivity(){
 
     private lateinit var auth: FirebaseAuth
-//    private lateinit var database: FirebaseFirestore
+    private lateinit var database: FirebaseFirestore
     private lateinit var nama : EditText
     private lateinit var email : EditText
     private lateinit var telp : EditText
@@ -41,64 +41,64 @@ class RegisActivity : AppCompatActivity(){
         }
         btnRegis = findViewById(R.id.button_daftar)
         btnRegis.setOnClickListener {
-            val nama = nama.text.toString().trim()
-            val email = email.text.toString().trim()
-            val telp = telp.text.toString().trim()
-            val pass = pass.text.toString().trim()
-
-            if(nama.isEmpty()){
-                Toast.makeText(this@RegisActivity, "Isi nama toko anda!", Toast.LENGTH_SHORT).show()
-            }else if(telp.isEmpty()){
-                Toast.makeText(this@RegisActivity, "Isi nomor telepon anda!", Toast.LENGTH_SHORT).show()
-            }else if(email.isEmpty()){
-                Toast.makeText(this@RegisActivity, "Isi email toko anda!", Toast.LENGTH_SHORT).show()
-            }else if(pass.isEmpty()){
-                Toast.makeText(this@RegisActivity, "Isi kata sandi anda!", Toast.LENGTH_SHORT).show()
-            }
-
-            auth.createUserWithEmailAndPassword(email, pass)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d("debug", "data berhasil dibuat!")
-                        val user = auth.currentUser
-//                    saveDataUser()
-                        Intent(this@RegisActivity, LoginActivity::class.java).also {
-                            startActivity(it)
-                        }
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w("debug", "data gagal dibuat!", task.exception)
-                        Toast.makeText(baseContext, "Gagal autentikasi.",
-                            Toast.LENGTH_SHORT).show()
-                    }
-                }
+            createUser()
         }
 
     }
 
-//    private fun createUser(){
-//
-//    }
+    private fun createUser(){
+        val nama = nama.text.toString().trim()
+        val email = email.text.toString().trim()
+        val telp = telp.text.toString().trim()
+        val pass = pass.text.toString().trim()
 
-//    private fun saveDataUser(){
-//        database = FirebaseFirestore.getInstance()
-//
-//        val hashMap :HashMap<String, Any> = HashMap()
-//        hashMap["nama_toko"] = nama.text.toString()
-//        hashMap["no_telp"] = telp.text.toString()
-//
-//
-//
-//        database.collection("data_toko").document().set(hashMap)
-//            .addOnSuccessListener {
-//                Toast.makeText(this,"Data sukses dibuat",Toast.LENGTH_SHORT).show()
-//            }
-//            .addOnFailureListener {
-//                Toast.makeText(this,"Data gagal dibuat", Toast.LENGTH_SHORT).show()
-//            }
-//
-//    }
+        if(nama.isEmpty()){
+            Toast.makeText(this@RegisActivity, "Isi nama toko anda!", Toast.LENGTH_SHORT).show()
+        }else if(telp.isEmpty()){
+            Toast.makeText(this@RegisActivity, "Isi nomor telepon anda!", Toast.LENGTH_SHORT).show()
+        }else if(email.isEmpty()){
+            Toast.makeText(this@RegisActivity, "Isi email toko anda!", Toast.LENGTH_SHORT).show()
+        }else if(pass.isEmpty()){
+            Toast.makeText(this@RegisActivity, "Isi kata sandi anda!", Toast.LENGTH_SHORT).show()
+        }
+
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("debug", "data berhasil dibuat!")
+                    val user = auth.currentUser
+                    saveDataUser()
+                    Intent(this@RegisActivity, LoginActivity::class.java).also {
+                        startActivity(it)
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("debug", "data gagal dibuat!", task.exception)
+                    Toast.makeText(baseContext, "Gagal autentikasi.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun saveDataUser(){
+        database = FirebaseFirestore.getInstance()
+
+        val hashMap :HashMap<String, Any> = HashMap()
+        hashMap["nama_toko"] = nama.text.toString()
+        hashMap["no_telp"] = telp.text.toString()
+
+
+
+        database.collection("data_toko").document().set(hashMap)
+            .addOnSuccessListener {
+                Toast.makeText(this,"Data sukses dibuat",Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this,"Data gagal dibuat", Toast.LENGTH_SHORT).show()
+            }
+
+    }
 }
 
 
